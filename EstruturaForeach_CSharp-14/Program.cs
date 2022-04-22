9df2ef67aee8194607b49240008fec36df299f95
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 22:47:21 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaForeach_CSharp-14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstruturaForeach_CSharp_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para trabalhar com vetores e matrizes
+
+            string[] nomes = new string[] { "Maria", "João", "Joaquina", "Pedro" };
+
+            foreach(string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
